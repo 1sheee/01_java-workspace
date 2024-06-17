5001d130b49d5fb42e2da6e7da28c8a627f16aba
1sheee <wiriwmfz@naver.com>
2024-06-17 17:38:00 +0900

    첫 프로젝트 생성
    FirstClass 만들기

---
+++ 01_MyFirstProject/src/com/br/first/FirstClass.java
@@ -0,0 +1,42 @@
+package com.br.first;
+
+public class FirstClass {
+
+	// single-line comment(주석) : 소스코드와 무관한 내용을 기술한다. / 코드로써 인직되지 않음.	
+	
+	/*
+	 * multi-line comment
+	 * 여러줄 주석
+	 * 
+	 * 1. 프로젝트 만들기
+	 * 2. 패키지 만들기
+	 * 3. 클래스 만들기
+	 * 4. 클래스 내에 소스코드 작성
+	 * 
+	 * 프로젝트
+	 * 		ㄴ 패지키1
+	 * 			ㄴ 클래스1_1
+	 * 			ㄴ 클래스1_2
+	 * 				...
+	 * 			ㄴ 패키지1_2
+	 * 		ㄴ 패키지2
+	 * 			ㄴ 패키지2_1
+	 * --------------------------------------
+	 * 
+	 * public class 클래스 명 {
+	 *		// 메소드 == 기능 
+	 *  }
+	 *  
+	 *  
+	 *  	메인메소드(실행용메소드) {
+	 *  		소스코드;
+	 *  }
+	 *  
+	 */
+	
+	public static void main(String[] args) {
+		
+			System.out.println(1);
+			System.out.println(2);
+	}
+}
