d888e1bb8726d3612c0a70e6df76eeefaf49e23a
1sheee <wiriwmfz@naver.com>
2024-06-18 17:39:19 +0900

    2일차 - 런 클래스

---
+++ 02_Variable/src/com/br/run/RunClass.java
@@ -0,0 +1,20 @@
+package com.br.run;
+
+import com.br.variable.Variable;
+import com.br.variable.Keyboardinput;
+
+public class RunClass {
+	
+	public static void main(String[] args) {
+		
+		Variable var = new Variable ();
+		//var.whyUseVariable();
+		//~~
+		//var.constant();
+		
+		Keyboardinput key = new Keyboardinput();
+		key.inputTest1();
+		
+	}
+
+}
