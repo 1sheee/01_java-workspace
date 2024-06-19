380525e0e73280d137694732e67795402f65a9d6
1sheee <wiriwmfz@naver.com>
2024-06-19 15:35:35 +0900

    3일차 - 런클래스

--- 02_Variable/src/com/br/run/RunClass.java
+++ 02_Variable/src/com/br/run/RunClass.java
@@ -2,6 +2,9 @@ package com.br.run;
 
 import com.br.variable.Variable;
 import com.br.variable.Keyboardinput;
+import com.br.variable.Printf;
+import com.br.variable.Casting;
+//import com.br.variable.*; - 베리어블의 모든걸 쓰겠다
 
 public class RunClass {
 	
@@ -13,7 +16,21 @@ public class RunClass {
 		//var.constant();
 		
 		Keyboardinput key = new Keyboardinput();
-		key.inputTest1();
+		//key.inputTest1();
+		//key.preactice();
+		//key.inputTest3();
+		
+		//key.chatAtTest();
+		
+		//key.practice2();
+		
+		//Printf pf = new Printf();
+		//pf.printfTest();
+		//key.inputTest2();
+		
+		Casting cast = new Casting();
+		//cast.autoCasting();
+		cast.forceCasting();
 		
 	}
 
