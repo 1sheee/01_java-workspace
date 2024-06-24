cc9af81e71c01f621514240a9a357068294d32f8
1sheee <wiriwmfz@naver.com>
2024-06-24 17:21:29 +0900

    반복문 개념(6일차)

---
+++ 04_Control/src/com/br/chap02/practice/example/LoopPractice.java
@@ -0,0 +1,11 @@
+package com.br.chap02.practice.example;
+
+public class LoopPractice {
+
+	public void practice1() {
+		
+	}
+	
+	}
+
+
---
+++ 04_Control/src/com/br/chap02/practice/run/Run.java
@@ -0,0 +1,17 @@
+package com.br.chap02.practice.run;
+
+import com.br.chap02.practice.example.*;
+
+
+public class Run {
+
+	public static void main(String[] args) {
+		
+		LoopPractice lp = new LoopPractice();
+		
+		
+		
+
+	}
+
+}
