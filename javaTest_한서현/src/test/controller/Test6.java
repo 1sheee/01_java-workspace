92c754c5673cf22eac104a2695d130ea8b75ffa5
1sheee <wiriwmfz@naver.com>
2024-06-26 22:19:34 +0900

    쪽지시험 (8일차)
    2, 6번 문제 해결

--- javaTest_한서현/src/test/controller/Test6.java
+++ javaTest_한서현/src/test/controller/Test6.java
@@ -4,18 +4,26 @@ public class Test6 {
 
 	public static void main(String[] args) {
 		
-		int[] array = {1,4,71,23,6,8,19,201,12,11};
-				
-		for(int i = 0; i < array.length; i++) {
-			if(i % 2 == 1) {
-			continue;
-					
-			}
-			System.out.println(array[i]);
+		int[] array = {1, 4, 71, 23, 6, 8, 19, 201, 12, 11};
 		
+		int sum = 0;		//합계 담아줄
+		int count = 0;		//갯수 카운트
+		
+		for(int i = 0; i<array.length; i++) {
+			if(array[i] % 2 ==0) {
+				continue;
+			}
+			sum += array[i];
+			count++;
+						
+			System.out.println(array[i]);
+		}
+			System.out.println("더해진 갯수 : " + count);
+			System.out.println("합계 : " + sum);
+			System.out.printf("평균 : %.1f", sum / (double)(count));
 					
 
 	}
 
 }
-}
+
