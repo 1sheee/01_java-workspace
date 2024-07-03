5dd871eee3dba6310cd2c36e2b23716cff026863
1sheee <wiriwmfz@naver.com>
2024-07-03 17:43:21 +0900

    상속 실습문제 (13일차)
    1번 문제 푸는 중

---
+++ 08_Inheritance_Homework_한서현/src/com/hw1/model/vo/Employee.java
@@ -0,0 +1,31 @@
+package com.hw1.model.vo;
+
+public class Employee extends Person{
+	
+	private int salary;
+	private String dept;
+	public Employee(String name, int age, double height, double weight,int salary, String dept) {
+		super(age,height,weight);
+		this.name = name;
+		this.salary = salary;
+		this.dept = dept;
+	}
+	public int getSalary() {
+		return salary;
+	}
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+	public String getDept() {
+		return dept;
+	}
+	public void setDept(String dept) {
+		this.dept = dept;
+	}
+	public String toString() {
+		return super.toString() + "salary" + salary + "dept" + dept;
+				
+	}
+	
+	
+}
